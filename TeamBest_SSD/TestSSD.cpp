6081b50304c3878e44631c395ff1d02110a75b93
DESKTOP-5FBE16T\User <rhaktjsrytk@naver.com>
2025-05-09 16:05:58 +0900

    [Feature] Initialize 함수 추가 변경점 - ssd_nand.txt 파일 생성 검증 [V] Build [V] Regression Test Pass [V] 해당 기능 TC

--- TeamBest_SSD/TestSSD.cpp
+++ TeamBest_SSD/TestSSD.cpp
@@ -8,3 +8,18 @@ using namespace testing;
 TEST(TestSSD, ContructorTest) {
 	EXPECT_NO_THROW(std::shared_ptr<SSD> ssd = std::make_shared<SSD>());
 }
+
+TEST(TestSSD, TestFileExistsAfterInit) {
+    SSD app;
+    const std::string filename = "ssd_nand.txt";
+
+    // 사전 정리: 파일이 있으면 삭제
+    if (std::filesystem::exists(filename)) {
+        std::filesystem::remove(filename);
+    }
+
+    app.Initialize(filename);
+
+    // 검증: 파일이 생성되었는지 확인
+    EXPECT_TRUE(std::filesystem::exists(filename));
+}
--- TeamBest_SSD/ssd.cpp
+++ TeamBest_SSD/ssd.cpp
@@ -1,6 +1,14 @@
 #include "ssd.h"
 
-void SSD::Read(int lba){
+void SSD::Initialize(const std::string& fileName) {
+    std::ofstream outFile(fileName);
+    if (outFile.is_open()) {
+        outFile << "example content\n";
+        outFile.close();
+    }
+}
+
+void SSD::Read(int lba) {
 
 }
 
