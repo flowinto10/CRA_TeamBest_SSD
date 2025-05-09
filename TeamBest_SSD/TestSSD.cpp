b21d0595f2c7f08818f196ea271c4c8a7a40130a
youngchul.hwang <sofe072@gmail.com>
2025-05-09 14:52:49 +0900

    Commit 제목 : [Refactor] SSD 생성 테스트 코드 추가 Commit 내용 : SSD 생성 테스트 코드 추가 TestSSD.cpp에 SSD 생성 테스트 코드 검증 [+] Build [ ] Regrestion Test Pass [+] 해당 기능 TC

--- TeamBest_SSD/TestSSD.cpp
+++ TeamBest_SSD/TestSSD.cpp
@@ -1,7 +1,12 @@
 #include "gmock/gmock.h"
+#include "ssd.h"
+
+#include <memory>
 
 using namespace testing;
 
-TEST(TestSSD, read1) {
+TEST(TestSSD, ContructorTest) {
+	std::shared_ptr<SSD> ssd = std::make_shared<SSD>();
+
 	EXPECT_EQ(1, 1);
 }
