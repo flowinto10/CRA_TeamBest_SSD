f3af9d20374d175b99fc5b265909389add61b496
DESKTOP-5FBE16T\User <rhaktjsrytk@naver.com>
2025-05-09 16:22:48 +0900

    [Refactor] Initialize 함수 추가 변경점 - else문 정리 검증 [V] Build [V] Regression Test Pass [V] 해당 기능 TC

--- TeamBest_SSD/ssd.cpp
+++ TeamBest_SSD/ssd.cpp
@@ -3,17 +3,14 @@
 const int FILESIZE = 99;
 
 void SSD::Initialize(const std::string& fileName) {
+    std::ofstream outFile(fileName);
     if (fileName == "ssd_nand.txt") {
-        std::ofstream outFile(fileName);
         if (outFile.is_open()) {
             for (int i = 0; i <= FILESIZE; ++i) {
                 outFile << i << " 0x00000000\n";
             }
             outFile.close();
         }
-    }
-    else {
-        std::ofstream outFile(fileName);
     }
 }
 
