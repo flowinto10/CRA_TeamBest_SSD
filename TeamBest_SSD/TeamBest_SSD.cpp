19ebf520c7c1b67c5dbb32ecedbda0ebeb1f5858
Youngjo Park <anyways5188@gmail.com>
2025-05-09 13:45:23 +0900

    Default Setting

---
+++ TeamBest_SSD/TeamBest_SSD.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include "gmock/gmock.h"
+
+using namespace testing;
+
+TEST(SSD_TS, TC0)
+{
+    EXPECT_EQ(0, 0);
+}
+
+int main()
+{
+    std::cout << "Hello CRA !\n";
+
+    ::testing::InitGoogleMock();
+    return RUN_ALL_TESTS();
+}
+
