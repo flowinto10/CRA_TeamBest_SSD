5de88be09546309f6739ed6dc01197889216247b
youngchul-hwang <sofe072@gmail.com>
2025-05-10 18:42:07 +0900

    [Feature] SSD::Write
    변경점
      SSD::Write(int lba, const std::string& value) 함수 구현
    
        주소와 value가 유효한 값인지 검사하고
        이상이 있으면 ssd_output.txt에 "ERROR"을 출력
    
        이상이 없으면 ssd_nand.txt의 모든 데이터를 읽고
        lba 위치의 값을 value로 변경한 뒤 ssd_nand.txt에 다시 저장
    
    검증
      [V] Build
      [V] Regression Test Pass
      [V] 해당 기능 TC

--- TeamBest_SSD/ssd.cpp
+++ TeamBest_SSD/ssd.cpp
@@ -1,4 +1,6 @@
 #include "ssd.h"
+#include <iostream>
+#include <regex>
 
 const int FILESIZE = 99;
 
@@ -18,6 +20,76 @@ void SSD::Read(int lba) {
 
 }
 
-void SSD::Write(int lba, const std::string& value) {
+void SSD::Write(int lba, const std::string& value) {    
+    if (!IsValidAddress(lba)) return WriteErrorMessageToOutputFile();
+    if (!IsValidValue(value)) return WriteErrorMessageToOutputFile();
+    if (!IsNandFileExist()) Initialize(NAND_FILE_PATH);
 
+    UpdateValueAtAddress(lba, value);
+}
+
+bool SSD::IsValidAddress(int address) {
+    return (address >= addressMin) && (address <= addressMax);
+}
+
+bool SSD::IsValidValue(const std::string& value) {
+    return std::regex_match(value, VALID_PATTERN);
+}
+
+bool SSD::IsNandFileExist() {
+    return std::filesystem::exists(NAND_FILE_PATH);
+}
+
+void SSD::WriteErrorMessageToOutputFile() {
+    std::ofstream outFile(OUTPUT_FILE_PATH, std::ios::trunc);
+    if (!outFile) {
+        std::cerr << "파일을 열 수 없습니다: " << OUTPUT_FILE_PATH << '\n';
+        return;
+    }
+
+    outFile << WRITE_ERROR_MESSAGE << '\n';
+    outFile.close();
+}
+
+void SSD::UpdateValueAtAddress(int lba, const std::string& value) {
+    std::vector<std::string> entireData = ReadDataForEntireAddress();
+    if (entireData.size() != GetEntireAddressSize()) {
+        std::cerr << "메모리 크기와 읽은 데이터의 수가 다릅니다!\n";
+        return;
+    }
+
+    std::string newDataAsSaveFormat = MakeDataAsSaveFormat(lba, value);
+    entireData[lba] = newDataAsSaveFormat;
+
+    WriteDataToEntireAddress(entireData);
+}
+
+std::vector<std::string> SSD::ReadDataForEntireAddress() {
+    std::vector<std::string> entireData;    
+
+    std::ifstream inFile(NAND_FILE_PATH);
+    if (!inFile) {
+        std::cerr << "파일 열기 실패: " << NAND_FILE_PATH << '\n';
+        return entireData;
+    }
+
+    entireData.reserve(GetEntireAddressSize());
+    std::string line;
+    while (std::getline(inFile, line)) {
+        entireData.push_back(line);
+    }
+    inFile.close();
+
+    return entireData;
+}
+
+void SSD::WriteDataToEntireAddress(std::vector<std::string>& values) {
+    std::ofstream outFile(NAND_FILE_PATH);
+    
+    if (outFile.is_open()) {
+        for (const auto& value : values) {
+            outFile << value << '\n';
+        }        
+        outFile.close();
+    }    
 }
--- TeamBest_SSD/ssd.h
+++ TeamBest_SSD/ssd.h
@@ -2,6 +2,9 @@
 #include <string>
 #include <filesystem>
 #include <fstream>
+#include <utility>
+#include <string_view>
+#include <regex>
 
 class SSD{
 public:
@@ -11,6 +14,53 @@ public:
 	void Read(int lba);
 	void Write(int lba, const std::string& value);
 
+	void SetAddressRange(int addressMin, int addressMax);
+	std::pair<int, int> GetAddressRange( );
+
 private:
+	bool IsNandFileExist();
+	bool IsValidAddress(int address);
+	bool IsValidValue(const std::string& value);	
+	
+	void UpdateValueAtAddress(int lba, const std::string& value);
+	std::vector<std::string> ReadDataForEntireAddress();
+	void WriteDataToEntireAddress(std::vector<std::string>& values);
+
+	void WriteErrorMessageToOutputFile();
+	size_t GetEntireAddressSize();
+	std::string MakeDataAsSaveFormat(int address, const std::string& data);
+
+private:
+	int addressMin{ ADDRESS_MIN_LIMIT };
+	int addressMax{ ADDRESS_MAX_LIMIT };
+
+	static constexpr int ADDRESS_MIN_LIMIT = 0;
+	static constexpr int ADDRESS_MAX_LIMIT = 99;
+
+	inline static const std::string NAND_FILE_PATH{"ssd_nand.txt"};
+	inline static const std::string OUTPUT_FILE_PATH{ "ssd_output.txt" };
+
+	const std::regex VALID_PATTERN{ "^0x[0-9A-F]{8}$" };;
+	inline static const std::string WRITE_ERROR_MESSAGE{ "ERROR" };
+
 };
 
+
+inline void SSD::SetAddressRange(int addrMin, int addrMax) {
+	if (addrMax < addrMin) return;
+
+	this->addressMin = std::max(addrMin, ADDRESS_MIN_LIMIT);
+	this->addressMax = std::min(addrMax, ADDRESS_MAX_LIMIT);
+}
+
+inline std::pair<int, int> SSD::GetAddressRange() {
+	return { addressMin, addressMax };
+}
+
+inline size_t SSD::GetEntireAddressSize() {
+	return static_cast<size_t>(addressMax - addressMin + 1);
+}
+
+inline std::string SSD::MakeDataAsSaveFormat(int address, const std::string& data) {
+	return std::to_string(address) + " " + data;
+}
