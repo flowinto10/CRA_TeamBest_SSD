0a112174afa4a2a9186c2d37db06b5526f4192fb
JoBD <cbd902@naver.com>
2025-05-12 10:43:15 +0900

    [Feature] feature/SSDFactory
    변경점
    SSDFactory 클래스 및 테스트 코드 작성
    SSDFactory는 싱글톤으로 구현하고,
    SSDFactory::createSSD(string type)에서 type에 따라 적절한 SSD를 생성하여 반환
    현재 생성 가능한 타입은 "SSD" 하나만 있음.
    
    검증
    [V] Build
    [V] Regression Test Pass
    [V] 해당 기능 TC

---
+++ TeamBest_SSD/ssd_factory.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+#include "ssd.h"
+#include <memory>
+
+class SSDFactory{
+    public:
+        static SSDFactory& getInstance() {
+            static SSDFactory instance;
+            return instance;
+        }
+
+        std::shared_ptr<ISSD> createSSD(const std::string& type);
+
+private:
+    SSDFactory() = default;
+    ~SSDFactory() = default;
+
+    SSDFactory(const SSDFactory&) = delete;
+    SSDFactory& operator=(const SSDFactory&) = delete;
+};
+
