cf2bcd2a4a2a103a886bf28001c5ad43f369fc8c
JoBD <cbd902@naver.com>
2025-05-12 10:41:45 +0900

    SSDFactory 테스트 함수 작성 1. 유효하지 않는 타입 생성 2. 유효한 타입 생성(현재 유효한 타입은 SSD 하나만 있음)

---
+++ TeamBest_SSD/TestSSDFactory.cpp
@@ -0,0 +1,25 @@
+#include "gmock/gmock.h"
+#include "ssd_controller.h"
+#include "ssd.h"
+#include "ssd_factory.h"
+
+#include <typeinfo>
+
+using namespace std;
+using namespace testing;
+
+TEST(TestSSDFactory, NonExistType) {
+	SSDFactory& factory = SSDFactory::getInstance();
+
+	std::shared_ptr<ISSD> iAmNotRealSSD
+		= factory.createSSD("iAmNotRealSSD");
+	
+	EXPECT_EQ(nullptr, iAmNotRealSSD);
+}
+
+TEST(TestSSDFactory, FactorySSDType) {
+	SSDFactory& factory = SSDFactory::getInstance();
+
+	std::shared_ptr<ISSD> ssdType = factory.createSSD("SSD");
+	EXPECT_EQ(typeid(SSD), typeid(*ssdType));
+}
