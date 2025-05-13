8630eb6f7a896aca920d434773659b46e658f098
DESKTOP-5FBE16T\User <rhaktjsrytk@naver.com>
2025-05-13 13:49:26 +0900

    [Feature] InitBuffers 함수 추가
    변경점
    - buffer 디렉터리내에 파일이 5개가 없으면 empty파일 5개까지 생성
    
    검증
    [V] Build
    [V] Regression Test Pass
    [V] 해당 기능 TC

--- TeamBest_SSD/TestCommandBuffer.cpp
+++ TeamBest_SSD/TestCommandBuffer.cpp
@@ -5,6 +5,16 @@
 
 using namespace testing;
 
+// buffer 폴더 내의 파일 수 확인
+int countFilesInBuffer() {
+	int count = 0;
+	for (const auto& entry : std::filesystem::directory_iterator("buffer")) {
+		if (std::filesystem::is_regular_file(entry)) {
+			++count;
+		}
+	}
+	return count;
+}
 
 TEST(TestCommandBuffer, ContructorTest) {
 	EXPECT_NO_THROW(std::shared_ptr<CommandBuffer> buffer 
@@ -13,11 +23,23 @@ TEST(TestCommandBuffer, ContructorTest) {
 
 TEST(TestCommandBuffer, BufferExistsAfterInit){
 	const std::string dirName = "buffer";
-	// 사전 정리: 파일이 있으면 삭제
+	// 사전 정리: buffer 폴더가 있으면 강제 삭제
 	if (std::filesystem::exists(dirName)) {
-		std::filesystem::remove(dirName);
+		// buffer 폴더 내의 모든 파일 및 하위 디렉터리 삭제
+		std::filesystem::remove_all(dirName);
 	}
 	CommandBuffer buffer;
 	// buffer 폴더가 생성되었는지 확인
 	EXPECT_TRUE(std::filesystem::exists(dirName));
+}
+
+TEST(TestCommandBuffer, FilesExistAfterInit) {
+	const std::string dirName = "buffer";
+
+	/*if (std::filesystem::exists(dirName)) {
+		std::filesystem::remove(dirName);
+	}*/
+	CommandBuffer buffer;
+	int cnt = countFilesInBuffer();
+	EXPECT_EQ(cnt, 5);
 }
--- TeamBest_SSD/command_buffer.cpp
+++ TeamBest_SSD/command_buffer.cpp
@@ -1,6 +1,7 @@
 ﻿#include "command_buffer.h"
 #include <string>
 #include <vector>
+#include <iostream>
 
 CommandBuffer::CommandBuffer() {
 	InitBuffers();
@@ -22,6 +23,27 @@ void CommandBuffer::InitBuffers() {
 	// buffer 폴더가 없으면 생성
 	if (!std::filesystem::exists("buffer")) {
 		std::filesystem::create_directory("buffer");
+	}
+	// buffer 폴더 안에 있는 파일들의 개수를 셈
+	int fileCount = 0;
+	for (const auto& entry : std::filesystem::directory_iterator("buffer")) {
+		if (std::filesystem::is_regular_file(entry)) {
+			fileCount++;
+		}
+	}
+	std::cout << "fileCount: " << fileCount << std::endl;
+	for (int i = fileCount+1; i <= 5; ++i) {
+		std::string fileName = "buffer/" + std::to_string(i) + "_empty";
+		std::cout << "fileName: " << fileName << std::endl;
+		std::ofstream outFile(fileName);
+		if (outFile) {
+			//outFile << "This is example file " << i << std::endl;
+			outFile.close();
+			std::cout << fileName << " 파일을 생성했습니다.\n";
+		}
+		else {
+			std::cout << fileName << " 파일을 생성하는 데 실패했습니다.\n";
+		}
 	}
 }
 
